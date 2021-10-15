7dcdb943bc3af15f6a36c9bc2ab54224af0f5fc7
Matthew Rideout <matthew@rideout.studio>
2021-10-15 13:25:22 -0400

    Improvements to handling language-only platform locale codes.

--- lib/models/locale/platform_locale/platform_locale_web.dart
+++ lib/models/locale/platform_locale/platform_locale_web.dart
@@ -9,8 +9,14 @@ class PlatformLocaleWeb implements PlatformLocale {
   @override
   Locale getPlatformLocale() {
     var _platformLocaleName = html.window.navigator.language;
-    print("language: " + _platformLocaleName);
+    print("Platform Locale Name (WEB): " + _platformLocaleName);
 
+    // Language code only
+    if (_platformLocaleName.length == 2) {
+      return Locale.fromSubtags(languageCode: _platformLocaleName);
+    }
+
+    // Language and country codes
     String _languageCode = _platformLocaleName.substring(0, _platformLocaleName.indexOf('-'));
     String _countryCode = _platformLocaleName.substring(_platformLocaleName.indexOf('-') + 1);
 
